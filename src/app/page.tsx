50fed31dd19b6ea3990b6cb26e78b34cc80feaff
lib <lib.martinito@pm.me>
2024-09-20 15:12:33 +0800

    chore: remove border

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="container border-2 border-red-500 h-screen mx-auto flex flex-col px-8 sm:px-16">
+    <div className="container h-screen mx-auto flex flex-col px-8 sm:px-16">
       <div className="my-auto flex flex-col gap-16">
         <div className="tracking-widest text-6xl sm:text-8xl">yet another expense tracker</div>
         <div className="flex gap-8 flex-col sm:flex-row">
