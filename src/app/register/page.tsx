e72ca697e9c531782818b28e8b46c839b1bd977d
lib <lib.martinito@pm.me>
2024-09-20 17:05:01 +0800

    feat: add register page

---
+++ src/app/register/page.tsx
@@ -0,0 +1,128 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const formSchema = z.object({
+  username: z.string().min(3).max(30),
+  email: z.string().email(),
+  password: z.string().min(3),
+  passwordConfirmation: z.string().min(3)
+}).refine((data) => data.password === data.passwordConfirmation, {
+  message: "Passwords don't match",
+  path: ["passwordConfirmation"]
+});
+
+export default function Login() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      email: "",
+      password: "",
+      passwordConfirmation: ""
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
+  return (
+    <div className="container mx-auto flex h-screen max-w-3xl flex-col border border-red-500 px-8 sm:px-16">
+      <div className="my-auto flex flex-col gap-16">
+        <div className="text-center text-6xl sm:text-8xl">register</div>
+
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="flex flex-col gap-6"
+          >
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input placeholder="username" {...field} />
+                  </FormControl>
+
+                  <FormDescription>what you&apos;d want to be called</FormDescription>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input placeholder="email" {...field} />
+                  </FormControl>
+
+                  <FormDescription>your unique identifier</FormDescription>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input placeholder="password" {...field} />
+                  </FormControl>
+
+                  <FormDescription>
+                    how you let us know you&apos;re you
+                  </FormDescription>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="passwordConfirmation"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input placeholder="password" {...field} />
+                  </FormControl>
+
+                  <FormDescription>
+                    just to be sure
+                  </FormDescription>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <Button type="submit" className="mt-6">
+              register
+            </Button>
+          </form>
+        </Form>
+      </div>
+    </div>
+  );
+}
