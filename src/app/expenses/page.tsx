80d2b9ce7d55e502c679d70c2bf9733f00c15d68
lib <lib.martinito@pm.me>
2024-09-22 13:31:25 +0800

    feat: add mock for page header

--- src/app/expenses/page.tsx
+++ src/app/expenses/page.tsx
@@ -1,15 +1,56 @@
 import { Button } from "@/components/ui/button"
 import { Pagination, PaginationContent, PaginationEllipsis, PaginationItem, PaginationLink, PaginationNext, PaginationPrevious } from "@/components/ui/pagination"
+import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
 
 export default async function Expenses() {
   const response = await fetch("http://127.0.0.1:4010/expenses").then((res) => res.json())
 
   return (
-    <div className="container mx-auto px-8 max-w-3xl sm:px-16">
-      <div className="text-center text-6xl sm:text-8xl">expenses</div>
+    <div className="container mx-auto px-8 sm:px-16">
+      <div className="flex justify-between items-center">
+        <div className="text-3xl">expenses</div>
+        <div className="flex gap-4">
+          <Select>
+            <SelectTrigger>
+              <SelectValue placeholder="month" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectItem value="january">january</SelectItem>
+                <SelectItem value="february">february</SelectItem>
+                <SelectItem value="march">march</SelectItem>
+                <SelectItem value="april">april</SelectItem>
+                <SelectItem value="may">may</SelectItem>
+                <SelectItem value="june">june</SelectItem>
+                <SelectItem value="july">july</SelectItem>
+                <SelectItem value="august">august</SelectItem>
+                <SelectItem value="september">september</SelectItem>
+                <SelectItem value="october">october</SelectItem>
+                <SelectItem value="november">november</SelectItem>
+                <SelectItem value="december">december</SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
 
-      <Table className="mt-16">
+          <Select>
+            <SelectTrigger>
+              <SelectValue placeholder="year" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectItem value="2024">2024</SelectItem>
+                <SelectItem value="2023">2023</SelectItem>
+                <SelectItem value="2022">2022</SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+
+          <Button>create</Button>
+        </div>
+      </div>
+
+      <Table className="mt-8">
         <TableHeader>
           <TableRow>
             <TableHead>item</TableHead>
