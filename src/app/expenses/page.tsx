0a0cd4280a4a66bb3e63d499e19a4b47880cbcb4
lib <lib.martinito@pm.me>
2024-09-22 12:54:00 +0800

    feat: add mock for pagination

--- src/app/expenses/page.tsx
+++ src/app/expenses/page.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button"
+import { Pagination, PaginationContent, PaginationEllipsis, PaginationItem, PaginationLink, PaginationNext, PaginationPrevious } from "@/components/ui/pagination"
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
 
 export default async function Expenses() {
@@ -8,7 +9,7 @@ export default async function Expenses() {
     <div className="container mx-auto px-8 max-w-3xl sm:px-16">
       <div className="text-center text-6xl sm:text-8xl">expenses</div>
 
-      <Table>
+      <Table className="mt-16">
         <TableHeader>
           <TableRow>
             <TableHead>item</TableHead>
@@ -31,6 +32,26 @@ export default async function Expenses() {
           ))}
         </TableBody>
       </Table>
+
+      <Pagination className="pt-8">
+        <PaginationContent>
+          <PaginationItem>
+            <PaginationPrevious href="#" />
+          </PaginationItem>
+
+          <PaginationItem>
+            <PaginationLink href="#">1</PaginationLink>
+          </PaginationItem>
+
+          <PaginationItem>
+            <PaginationEllipsis />
+          </PaginationItem>
+
+          <PaginationItem>
+            <PaginationNext href="#" />
+          </PaginationItem>
+        </PaginationContent>
+      </Pagination>
     </div>
   )
 }
