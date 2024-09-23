4de54a2a19c2104742d90e2b1922c0d381dd86f6
lib <lib.martinito@pm.me>
2024-09-23 20:44:46 +0800

    feat: add functionality to register, login, and logout

---
+++ src/components/header.tsx
@@ -0,0 +1,38 @@
+"use client"
+
+import { Button } from "./ui/button"
+import { removeToken } from "@/utils/auth"
+import { useRouter } from "next/navigation"
+import Link from "next/link"
+
+export default function Header() {
+  const router = useRouter()
+
+  const logout = async () => {
+    const response = await fetch("http://localhost:3000/v1/logout", { method: "POST" })
+
+    if (response.ok) {
+      removeToken()
+      router.push("/")
+    } else {
+      console.error("something went wrong: ", response)
+    }
+  }
+
+  return (
+    <div className="my-8 flex items-center justify-between">
+      <nav>
+        <ul className="flex gap-4">
+          <li>
+            <Link href="/expenses">expenses</Link>
+          </li>
+          <li>
+            <Link href="#">user</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Button variant="secondary" onClick={logout}>logout</Button>
+    </div>
+  )
+}
